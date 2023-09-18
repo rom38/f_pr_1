1fbf47e66878faf2e6d13b663da66d81de205bba
Rman_stat <roman_imp@mail.ru>
2023-09-18 13:49:59 +0000

    upgrade

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,7 @@ import "../styles/Main.css";
 import Countries from "./Countries";
 import Alerts from "./Alerts";
 import TownSelect from "./TownSelect";
+import RegionSelect from "./RegionSelect";
 
 function Main() {
     const alertText = "Ужас!";
@@ -11,6 +12,8 @@ function Main() {
             <h1>Hello World</h1>
             <Alerts>{alertText}</Alerts>
             <TownSelect/>
+            <RegionSelect/>
+
             <Countries />
         </main>
     );
--- src/components/TownSelect.jsx
+++ src/components/TownSelect.jsx
@@ -1,36 +1,53 @@
 import { Component, Fragment, useState } from "react";
 import { Button } from "react-bootstrap";
 import PropTypes, { array, object } from "prop-types";
-import "../styles/Country.css";
+import "../styles/TownSelect.css";
 import towns from "../data/towns.json";
 
 function TownSelect(props) {
     towns.sort(function(a,b) {return (a["city"] > b["city"]) ? 1 : ((b["city"] > a["city"]) ? -1 : 0);} );
-    
+    const townsIdDict = {};
+    towns.map((item)=>{return townsIdDict[item["fias_id"]]=item;});
+    //[coords, setCoords] = useState([]);
+    const defaultTown = "Москва";
+
     const uniqueRegions = [...new Set(towns.map((item) => item["Region"]))];
     const uniqueFederals = [
         ...new Set(towns.map((item) => item["federal_district"])),
     ];
     function onSelect(event) {
-        //const selectedIndex = event.target.options.selectedIndex;
-        //console.log(event.target.options[selectedIndex]);
-        console.log("event from input", event.target.key);
+        const selectedIndex = event.target.options.selectedIndex;
+        console.log(event.target.options[selectedIndex]);
+        const idTownSelected = event.target.options[selectedIndex].id;
+        console.log("event from input id", idTownSelected);
+        console.log("coords", `${townsIdDict[idTownSelected]["geo_lat"]} ${townsIdDict[idTownSelected]["geo_lon"]}` )
+        // console.log("event from input", event.target.value);
+
+
     }
     //const [selected, changeSelected] = useState(false);
     return (
         <ul>
-            <li>jkfghjksdfsdkjf</li>
-            <input
+            <label className="search-label">Укажите ваш город, в поле работает поиск:</label>
+            {/* <input
                 list="towns"
                 name="townsChoice"
                 placeholder="Город"
                 onChange={onSelect}
+                defaultValue={defaultTown}
             />
             <datalist id="towns">
                 {towns.map((town) => (
                     <option key={town["fias_id"]} value={town["city"]}></option>
                 ))}
-            </datalist>
+            </datalist> */}
+            <select name="town-select" id="town-select" onChange={onSelect}>
+                {towns.map((town) => (
+                    <option key={town["fias_id"]} id={town["fias_id"]}>{town["city"]}</option>
+                ))}
+            </select>
+            <label>координаты</label>
+
             {towns.slice(1, 7).map((town) => (
                 <li key={town["fias_id"]}>{town["city"]}</li>
             ))}
