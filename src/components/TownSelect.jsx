668ab5c71b4c8937d6e4405ec3efef855c31763e
Rman_stat <roman_imp@mail.ru>
2023-09-18 09:08:04 +0000

    upgrade to new base

--- src/components/TownSelect.jsx
+++ src/components/TownSelect.jsx
@@ -5,9 +5,11 @@ import "../styles/Country.css";
 import towns from "../data/towns.json";
 
 function TownSelect(props) {
-    const uniqueRegions = [...new Set(towns.map((item) => item["Регион"]))];
+    towns.sort(function(a,b) {return (a["city"] > b["city"]) ? 1 : ((b["city"] > a["city"]) ? -1 : 0);} );
+    
+    const uniqueRegions = [...new Set(towns.map((item) => item["Region"]))];
     const uniqueFederals = [
-        ...new Set(towns.map((item) => item["Федеральный округ"])),
+        ...new Set(towns.map((item) => item["federal_district"])),
     ];
     function onSelect(event) {
         //const selectedIndex = event.target.options.selectedIndex;
@@ -26,11 +28,11 @@ function TownSelect(props) {
             />
             <datalist id="towns">
                 {towns.map((town) => (
-                    <option key={town["lat"]} value={town["Город"]}></option>
+                    <option key={town["fias_id"]} value={town["city"]}></option>
                 ))}
             </datalist>
-            {towns.map((town) => (
-                <li key={town["lat"]}>{town["Город"]}</li>
+            {towns.slice(1, 7).map((town) => (
+                <li key={town["fias_id"]}>{town["city"]}</li>
             ))}
         </ul>
     );
